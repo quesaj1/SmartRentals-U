626a304ce575cb4ebd12f883e73e7271da1695c2
ransrcri23 <ran.sanchez.r@gmail.com>
2020-06-21 17:21:36 -0600

    Packages commit
    Packages commit

--- ProyectoSmartRentals/ViewSwitcher.ascx.cs
+++ ProyectoSmartRentals/ViewSwitcher.ascx.cs
@@ -1,7 +1,11 @@
-using Microsoft.AspNet.FriendlyUrls.Resolvers;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.FriendlyUrls.Resolvers;
 
 namespace ProyectoSmartRentals
 {
