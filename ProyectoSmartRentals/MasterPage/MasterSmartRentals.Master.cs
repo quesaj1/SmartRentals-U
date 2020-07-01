6bc0505cc7a79b704b7e1360e886626ae8805164
Guillermo <65992561+guille7CR@users.noreply.github.com>
2020-06-30 23:38:29 -0600

    Update MasterSmartRentals.Master.cs
    Funcion para capitalizar primera letra del dia en la fecha y ajustes al formato ES

--- ProyectoSmartRentals/MasterPage/MasterSmartRentals.Master.cs
+++ ProyectoSmartRentals/MasterPage/MasterSmartRentals.Master.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 namespace ProyectoSmartRentals.MasterPage
 {
+
+  
     public partial class MasterSmartRentals : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
+
         {
-            DateDisplay.Text = DateTime.Now.ToString("dddd, dd MMMM yyyy", CultureInfo.GetCultureInfo("es-ES"));
+            string str ;
+            str = DateTime.Now.ToString("dddd, dd MMMM' de 'yyyy", CultureInfo.GetCultureInfo("es-ES"));
+            str= Char.ToUpper(str[0]) +str.Substring(1).ToString();
+            DateDisplay.Text = str;
         }
+
+
     }
 }
